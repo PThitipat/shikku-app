9e1aec9b7ed5d99b2763536d69d38bd4797f85ae
simple-hubs <simplehub.roblox@gmail.com>
2025-01-31 17:12:18 +0700

    asdasda44324

--- app/tracking/[username]/page.tsx
+++ app/tracking/[username]/page.tsx
@@ -41,134 +41,105 @@ interface PlayerData {
 }
 
 export default function UsersPage() {
-    const params = useParams();
-    const username = params?.username || "";
-    const [player, setPlayer] = useState<PlayerData | null>(null);
-    const [error, setError] = useState<string | null>(null);
-    const [searchQuery, setSearchQuery] = useState<string>("");
-    const [filterType, setFilterType] = useState<string>("All");
-    
+  const params = useParams();
+  const username = params?.username || "";
+  const [player, setPlayer] = useState<PlayerData | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [filterType, setFilterType] = useState<string>("All");
+
+  const fetchData = useCallback(async () => {
+    try {
+      const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/api/players_data?username=${username}`;
+      const response = await fetch(endpoint);
+      const result = await response.json();
+
+      if (result.success) {
+        setPlayer(result.data[0]); // Set single player
+      } else {
+        throw new Error(result.error || "Data fetch error");
+      }
+    } catch (err) {
+      console.error("Error fetching data:", err);
+      setError("Failed to fetch data");
+    }
+  }, [username]);
+
+  useEffect(() => {
     if (!process.env.NEXT_PUBLIC_API_URL) {
-        return null;
+      console.error("API URL is missing");
+      return;
+    }
+
+    fetchData();
+
+    const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`);
+
+    socket.on("connect", () => {
+      console.log("Connected to WebSocket:", socket.id);
+    });
+
+    socket.on("updateData", (data: PlayerData[]) => {
+      const updatedPlayer = data.find((player) => player.username === username);
+      if (updatedPlayer) setPlayer(updatedPlayer);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from WebSocket");
+    });
+
+    return () => {
+      socket.disconnect();
     };
-    
-    // Fetch all players or a specific player
-    const fetchData = useCallback(async () => {
-        try {
-          const endpoint = username
-            ? `${process.env.NEXT_PUBLIC_API_URL}/api/players_data?username=${username}`
-            : `${process.env.NEXT_PUBLIC_API_URL}/api/players_data`;
-      
-          const response = await fetch(endpoint);
-          const result = await response.json();
-      
-          if (result.success) {
-            if (username) {
-              setPlayer(result.data[0]); // Set single player
-            }
-          } else {
-            throw new Error(result.error || "Data fetch error");
-          }
-        } catch (err) {
-          console.error("Error fetching data:", err);
-          setError("Failed to fetch data");
-        }
-      }, [username]);      
+  }, [username, fetchData]);
 
-      useEffect(() => {
-        fetchData();
-      
-        const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`);
-      
-        socket.on("connect", () => {
-          console.log("Connected to WebSocket:", socket.id);
-        });
-      
-        socket.on("updateData", (data: PlayerData[]) => {
-          if (username) {
-            const updatedPlayer = data.find((player) => player.username === username);
-            if (updatedPlayer) setPlayer(updatedPlayer);
-          }
-        });
-      
-        socket.on("disconnect", () => {
-          console.log("Disconnected from WebSocket");
-        });
-      
-        return () => {
-          socket.disconnect();
-        };
-      }, [username, fetchData]);      
-      
+  return (
+    <div className="min-h-screen bg-[#0B0B13] text-white">
+      <div className="container mx-auto py-4 px-4 flex flex-col gap-6">
+        <h1 className="text-2xl font-bold">ติดตามสถานะ</h1>
+        <div className="flex gap-2 items-center">
+          <Input
+            type="text"
+            placeholder="Search username..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-[180px] text-white"
+          />
 
-    // Filter players if showing all
-    // const filteredPlayers = playersData.filter((player) => {
-    //     const matchesSearch = player.username
-    //     .toLowerCase()
-    //     .includes(searchQuery.toLowerCase());
+          <Select
+            value={filterType}
+            onValueChange={(value) => setFilterType(value)}
+          >
+            <SelectTrigger className="w-[180px] text-white">
+              <SelectValue placeholder="Filter" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Filter</SelectLabel>
+                <SelectItem value="All">
+                  <div className="flex items-center">
+                    <BiWorld className="mr-2 h-4 w-4" />
+                    All
+                  </div>
+                </SelectItem>
+                <SelectItem value="Online">
+                  <div className="flex items-center">
+                    <FaCircle className="mr-2 h-3 w-3 text-green-500" />
+                    Online
+                  </div>
+                </SelectItem>
+                <SelectItem value="Offline">
+                  <div className="flex items-center">
+                    <FaCircle className="mr-2 h-3 w-3 text-red-400" />
+                    Offline
+                  </div>
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
 
-    //     switch (filterType) {
-    //     case "Offline":
-    //         return matchesSearch && player.status === "Offline";
-    //     case "Online":
-    //         return matchesSearch && player.status === "Online";
-    //     default:
-    //         return matchesSearch;
-    //     }
-    // });
-
-    // Render single player
-    return (
-        <div className="min-h-screen bg-[#0B0B13] text-white">
-
-        <div className="container mx-auto py-4 px-4 flex flex-col gap-6">
-            <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
-                <h1 className="text-2xl font-bold">ติดตามสถานะ</h1>
-
-                <div className="flex gap-2 items-center">
-                    <Input
-                    type="text"
-                    placeholder="Search username..."
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    className="w-[180px] text-white"
-                    />
-
-                    <Select
-                    value={filterType}
-                    onValueChange={(value) => setFilterType(value)}
-                    >
-                    <SelectTrigger className="w-[180px] text-white">
-                        <SelectValue placeholder="Filter" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                        <SelectLabel>Filter</SelectLabel>
-                        <SelectItem value="All">
-                            <div className="flex items-center">
-                            <BiWorld className="mr-2 h-4 w-4" />
-                            All
-                            </div>
-                        </SelectItem>
-                        <SelectItem value="Online">
-                            <div className="flex items-center">
-                            <FaCircle className="mr-2 h-3 w-3 text-green-500" />
-                            Online
-                            </div>
-                        </SelectItem>
-                        <SelectItem value="Offline">
-                            <div className="flex items-center">
-                            <FaCircle className="mr-2 h-3 w-3 text-red-400" />
-                            Offline
-                            </div>
-                        </SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                    </Select>
-                </div>
-            </div>
-
-            {error && <p className="text-red-500">{error}</p>}
+        {error && <p className="text-red-500">{error}</p>}
 
         {player ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
@@ -193,8 +164,7 @@ export default function UsersPage() {
             <p className="text-gray-300">Player not found</p>
           </div>
         )}
-
-        </div>
+      </div>
     </div>
-    );
+  );
 }
